94032a4b20fa345a643e243d714807868d0d51eb
JAMES GAMBRAH <jamesgambrah@gmail.com>
2026-03-14 10:57:23 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:768581214945:web:d9f58237f41627cb074da0",
   "apiKey": "AIzaSyDaGJw8jtcW7GiYuiSL-a4F_vi-vnYO_e4",
   "authDomain": "studio-4153783954-c4961.firebaseapp.com",
+  "storageBucket": "studio-4153783954-c4961.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "768581214945"
 };
